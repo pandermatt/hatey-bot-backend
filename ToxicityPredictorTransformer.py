a133b98e40f728594e34164120a7141acfe5a6bb
Tobias Fankhauser <tobias7402@gmail.com>
2022-10-15 14:47:06 +0200

    ToxicityPredictor Filter options

--- ToxicityPredictorTransformer.py
+++ ToxicityPredictorTransformer.py
@@ -1,10 +1,11 @@
+# ToxicityDetection class
 from detoxify import Detoxify
 
 class ToxicityPredictorTransformer:
-    def __init__(self):
+    def __init__(self, filter_setting=['toxicity', 'severe_toxicity', 'obscene', 'threat', 'insult', 'identity_attack'], threshold=0.5):
         self.model = Detoxify('original')
-        self.threshold = 0.5
-
+        self.filter_setting = filter_setting
+        self.threshold = threshold
 
     def predict(self, sentence):
         prediction = self.model.predict(sentence)
@@ -19,3 +20,13 @@ class ToxicityPredictorTransformer:
         if clean:
             prediction['clean'] = 1
         return prediction
+
+    def applyFilter(self, prediction):
+        for key in self.filter_setting:
+            if prediction[key] == 1:
+                return False
+        return True
+
+    def isThisSentenceClean(self, sentence):
+        prediction = self.predict(sentence)
+        return self.applyFilter(prediction)
