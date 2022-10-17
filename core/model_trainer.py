b2f461169fbe42d128d8a2c0470394d0a558b83e
Pascal Andermatt <p.andermatt@me.com>
2022-10-17 16:18:59 +0200

    add more logger

--- core/model_trainer.py
+++ core/model_trainer.py
@@ -5,6 +5,7 @@ from data_model.toxic_comment_data import ToxicCommentData
 from model.ensemble_classifier import EnsembleClassifier
 from model.tokenizer import NLTKTokenizer
 from util.file_io import FileIo
+from util.logger import log
 
 
 def generate_and_train_model(dataset=None, tokenizer=None, base_classifier=None):
@@ -26,11 +27,13 @@ def generate_and_train_model(dataset=None, tokenizer=None, base_classifier=None)
                                   lambda x: tokenizer.tokenize(x))
     X = cached_tokens(X)
 
+    log.info("Training model...")
     if base_classifier is None:
         base_classifier = BaggingClassifier(base_estimator=ExtraTreesClassifier(),
                                             n_estimators=10, max_samples=0.5,
                                             max_features=0.5)
     model = EnsembleClassifier(classifier=base_classifier, label_names=dataset.get_label_names())
     model.train(X, Y)
+    log.info("Model trained.")
 
     return model, tokenizer
